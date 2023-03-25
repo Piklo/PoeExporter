f99fc64923c043a595bf21b5d4a2301e04dfe1a9
Piklo <16057987+Piklo@users.noreply.github.com>
2023-03-25 16:28:20 +0100

    added array extensions class with IndexOfSubArray method

---
+++ PoeData/Extensions/ArrayExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+
+namespace PoeData.Extensions;
+
+/// <summary>
+/// Array extensions.
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Finds the index of a sub array in a given array.
+    /// </summary>
+    /// <typeparam name="T">Type of both arrays.</typeparam>
+    /// <param name="array">array to look in.</param>
+    /// <param name="subArray">array to look for.</param>
+    /// <returns>Index of a sub array. -1 if not found.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if either of the arrays is null.</exception>
+    // https://stackoverflow.com/a/26880541
+    public static int IndexOfSubArray<T>(this T[] array, T[] subArray)
+        where T : IEqualityOperators<T, T, bool>
+    {
+        if (array is null)
+        {
+            throw new ArgumentNullException(nameof(array));
+        }
+
+        if (subArray is null)
+        {
+            throw new ArgumentNullException(nameof(subArray));
+        }
+
+        var len = subArray.Length;
+        var limit = array.Length - len;
+        for (var i = 0; i <= limit; i++)
+        {
+            var k = 0;
+            for (; k < len; k++)
+            {
+                if (subArray[k] != array[i + k])
+                {
+                    break;
+                }
+            }
+
+            if (k == len)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
