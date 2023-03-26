e39de62a3e9f7969797c42b15d23e4a3d2a96bf8
Piklo <16057987+Piklo@users.noreply.github.com>
2023-03-26 14:42:15 +0200

    added GetPossibleColumnTypes, GetSchemaStringAsync, GenerateSpecification, TableNameToClassName, GetColumnTypeData, GetColumnName, methods added ColumnTypeData struct

---
+++ SpecificationGenerator/ColumnTypeData.cs
@@ -0,0 +1,14 @@
+﻿namespace SpecificationGenerator;
+
+internal readonly struct ColumnTypeData
+{
+    public readonly required string Value { get; init; }
+
+    public readonly required string BaseColumnType { get; init; }
+
+    public readonly required bool IsUnknown { get; init; }
+
+    public readonly required bool IsArray { get; init; }
+
+    public readonly required bool IsForeignRow { get; init; }
+}
