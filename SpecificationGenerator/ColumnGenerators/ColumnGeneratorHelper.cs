fdc3610244055c8b27e105637a3e8896b705f9e9
Piklo <16057987+Piklo@users.noreply.github.com>
2023-03-28 18:08:38 +0200

    added ColumnGeneratorHelper

---
+++ SpecificationGenerator/ColumnGenerators/ColumnGeneratorHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SpecificationGenerator.ColumnGenerators;
+
+/// <summary>
+/// Class containing helper methods related to column generation.
+/// </summary>
+internal static class ColumnGeneratorHelper
+{
+    /// <summary>
+    /// Generates a name for unknown column.
+    /// </summary>
+    /// <param name="parsedColumns">a readonly collection of already parsed columns.</param>
+    /// <returns>string column name.</returns>
+    internal static string GetUnknownColumnName(ReadOnlyCollection<IParsedColumn> parsedColumns)
+    {
+        var totalOffset = 0;
+
+        foreach (var column in parsedColumns)
+        {
+            totalOffset += column.Offset;
+        }
+
+        var name = $"Unknown{totalOffset}";
+
+        return name;
+    }
+}
