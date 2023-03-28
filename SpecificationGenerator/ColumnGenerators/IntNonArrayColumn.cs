22c87d5df0b2b87a0d922337510783e9329557b8
Piklo <16057987+Piklo@users.noreply.github.com>
2023-03-28 18:42:26 +0200

    added IntNonArrayColumn

---
+++ SpecificationGenerator/ColumnGenerators/IntNonArrayColumn.cs
@@ -0,0 +1,58 @@
+﻿using SpecificationGenerator.SchemaJson;
+using System.Collections.ObjectModel;
+
+namespace SpecificationGenerator.ColumnGenerators;
+
+/// <summary>
+/// Class which parses the column which is an int and is not an array.
+/// </summary>
+internal sealed class IntNonArrayColumn : IParsedColumn
+{
+    /// <inheritdoc/>
+    public string ClassPropertyName { get; }
+
+    /// <inheritdoc/>
+    public string? ReferencedTable { get; }
+
+    /// <inheritdoc/>
+    public string LoadingPropertyName { get; }
+
+    /// <inheritdoc/>
+    public int Offset { get; } = 4;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntNonArrayColumn"/> class.
+    /// </summary>
+    /// <param name="column">column to parse.</param>
+    /// <param name="parsedColumns">a readonly collection of already parsed columns.</param>
+    public IntNonArrayColumn(Column column, ReadOnlyCollection<IParsedColumn> parsedColumns)
+    {
+        ClassPropertyName = column.Name is not null ? column.Name : ColumnGeneratorHelper.GetUnknownColumnName(parsedColumns);
+        LoadingPropertyName = ClassPropertyName.ToLower();
+        ReferencedTable = column.References?.Table;
+    }
+
+    /// <inheritdoc/>
+    public string[] GetPropertyStrings()
+    {
+        var strings = new string[]
+        {
+            $"/// <summary> Gets {ClassPropertyName}.</summary>",
+            $$"""public required int {{ClassPropertyName}} { get; init; }""",
+        };
+
+        return strings;
+    }
+
+    /// <inheritdoc/>
+    public string[] GetLoading()
+    {
+        var strings = new string[]
+        {
+            $"// loading {ClassPropertyName}",
+            $"(var {LoadingPropertyName}, offset) = SpecificationFileLoader.LoadInt(decompressedFile, offset);",
+        };
+
+        return strings;
+    }
+}
