e3740e7e1f9c32443f94f8b0e1f3879ff6ebd9ee
Piklo <16057987+Piklo@users.noreply.github.com>
2023-03-29 15:07:13 +0200

    added ArrayArrayColumn updated ParseColumn

---
+++ SpecificationGenerator/ColumnGenerators/ArrayArrayColumn.cs
@@ -0,0 +1,62 @@
+﻿using SpecificationGenerator.SchemaJson;
+using System.Collections.ObjectModel;
+
+namespace SpecificationGenerator.ColumnGenerators;
+
+/// <summary>
+/// Class which parses the column which is an array and is an array.
+/// </summary>
+/// <remarks>
+/// Only gods know what type array means, lets just assume it's array of ints for now.
+/// </remarks>
+internal sealed class ArrayArrayColumn : IParsedColumn
+{
+    /// <inheritdoc/>
+    public string ClassPropertyName { get; }
+
+    /// <inheritdoc/>
+    public string? ReferencedTable { get; }
+
+    /// <inheritdoc/>
+    public string LoadingPropertyName { get; }
+
+    /// <inheritdoc/>
+    public int Offset { get; } = 16;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayArrayColumn"/> class.
+    /// </summary>
+    /// <param name="column">column to parse.</param>
+    /// <param name="parsedColumns">a readonly collection of already parsed columns.</param>
+    public ArrayArrayColumn(Column column, ReadOnlyCollection<IParsedColumn> parsedColumns)
+    {
+        ClassPropertyName = column.Name is not null ? column.Name : ColumnGeneratorHelper.GetUnknownColumnName(parsedColumns);
+        LoadingPropertyName = ClassPropertyName.ToLower();
+        ReferencedTable = column.References?.Table;
+    }
+
+    /// <inheritdoc/>
+    public string[] GetPropertyStrings()
+    {
+        var strings = new string[]
+        {
+            $"/// <summary> Gets {ClassPropertyName}.</summary>",
+            $$"""public required ReadOnlyCollection<int> {{ClassPropertyName}} { get; init; }""",
+        };
+
+        return strings;
+    }
+
+    /// <inheritdoc/>
+    public string[] GetLoading()
+    {
+        var strings = new string[]
+        {
+            $"// loading {ClassPropertyName}",
+            $"(var temp{LoadingPropertyName}, offset) = SpecificationFileLoader.LoadIntArray(decompressedFile, offset, dataOffset);",
+            $"var {LoadingPropertyName} = temp{LoadingPropertyName}.AsReadOnly();",
+        };
+
+        return strings;
+    }
+}
