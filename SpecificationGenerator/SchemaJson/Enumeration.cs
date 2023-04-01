59ea12a6316889b6319cf301abc8bac3a3151a66
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-01 20:13:12 +0200

    added string docs to Enumeration

--- SpecificationGenerator/SchemaJson/Enumeration.cs
+++ SpecificationGenerator/SchemaJson/Enumeration.cs
@@ -2,14 +2,20 @@
 
 namespace SpecificationGenerator.SchemaJson;
 
+/// <summary>
+/// Class containing data about enumeration in schema json.
+/// </summary>
 public class Enumeration
 {
+    /// <summary>Gets name.</summary>
     [JsonPropertyName("name")]
     public required string Name { get; init; }
 
+    /// <summary>Gets indexing.</summary>
     [JsonPropertyName("indexing")]
     public required int Indexing { get; init; }
 
+    /// <summary>Gets enumerators.</summary>
     [JsonPropertyName("enumerators")]
     public required string[] Enumerators { get; init; }
 }
