56f8539cb2e315dfecd10896b4ecc187787dac1d
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-01 20:19:37 +0200

    made Schema props required and init only added string docs to Schema

--- SpecificationGenerator/SchemaJson/Schema.cs
+++ SpecificationGenerator/SchemaJson/Schema.cs
@@ -2,17 +2,24 @@
 
 namespace SpecificationGenerator.SchemaJson;
 
+/// <summary>
+/// Class containing data about all of schema json.
+/// </summary>
 public class Schema
 {
+    /// <summary>Gets version.</summary>
     [JsonPropertyName("version")]
-    public int Version { get; set; }
+    public required int Version { get; init; }
 
+    /// <summary>Gets creation timestamp.</summary>
     [JsonPropertyName("createdAt")]
-    public int CreatedAt { get; set; }
+    public required int CreatedAt { get; init; }
 
+    /// <summary>Gets tables.</summary>
     [JsonPropertyName("tables")]
-    public Table[] Tables { get; set; }
+    public required Table[] Tables { get; init; }
 
+    /// <summary>Gets enumerations.</summary>
     [JsonPropertyName("enumerations")]
-    public Enumeration[] Enumerations { get; set; }
+    public required Enumeration[] Enumerations { get; init; }
 }
