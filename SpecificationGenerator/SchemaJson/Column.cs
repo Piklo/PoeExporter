b4094174f3b879ee81b2089ea85f2db2315c9d45
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-01 20:08:52 +0200

    added string docs to Column

--- SpecificationGenerator/SchemaJson/Column.cs
+++ SpecificationGenerator/SchemaJson/Column.cs
@@ -2,35 +2,49 @@
 
 namespace SpecificationGenerator.SchemaJson;
 
+/// <summary>
+/// Class containing data about each column in the schema json file.
+/// </summary>
 public class Column
 {
+    /// <summary>Gets name.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; init; }
 
+    /// <summary>Gets description.</summary>
     [JsonPropertyName("description")]
     public string? Description { get; init; }
 
+    /// <summary>Gets a value indicating whether column is an array.</summary>
     [JsonPropertyName("array")]
     public bool Array { get; init; }
 
+    /// <summary>Gets type.</summary>
     [JsonPropertyName("type")]
     public required string Type { get; init; }
 
+    /// <summary>Gets a value indicating whether values in the column are unique.</summary>
     [JsonPropertyName("unique")]
     public bool Unique { get; init; }
 
+    /// <summary>Gets a value indicating whether values in the column are localized.</summary>
     [JsonPropertyName("localized")]
     public bool Localized { get; init; }
 
+    /// <summary>Gets info about referenced table by that column.</summary>
     [JsonPropertyName("references")]
     public References? References { get; init; }
 
+    /// <summary>Gets until.</summary>
+    /// <remarks>always null?.</remarks>
     [JsonPropertyName("until")]
     public object? Until { get; init; }
 
+    /// <summary>Gets file extension.</summary>
     [JsonPropertyName("file")]
     public string? File { get; init; }
 
+    /// <summary>Gets files extensions.</summary>
     [JsonPropertyName("files")]
     public string[]? Files { get; init; }
 }
