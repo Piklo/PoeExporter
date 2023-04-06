a327ba7efdb6dda9067bdb1a7a2d0ac1ed1340ca
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-06 12:46:59 +0200

    moved IReadGgpkTagRecord to its own file

--- PoeData/Ggpk/GgpkRecords/IGgpkTagRecord.cs
+++ PoeData/Ggpk/GgpkRecords/IGgpkTagRecord.cs
@@ -11,20 +11,3 @@ internal interface IGgpkTagRecord
     /// <summary>Gets offset.</summary>
     public long Offset { get; init; }
 }
-
-/// <summary>
-/// Interface containing Read method for ggpk tag records.
-/// </summary>
-/// <typeparam name="T">type of the returned <see cref="IGgpkTagRecord"/>.</typeparam>
-internal interface IReadGgpkTagRecord<T>
-    where T : IGgpkTagRecord
-{
-    /// <summary>
-    /// Reads ggpk record data.
-    /// </summary>
-    /// <param name="ggpkReader">reader to read from.</param>
-    /// <param name="length">records length.</param>
-    /// <param name="offset">records offset.</param>
-    /// <returns>parsed ggpk record data.</returns>
-    public static abstract T Read(BinaryReader ggpkReader, int length, long offset);
-}
---
+++ PoeData/Ggpk/GgpkRecords/IReadGgpkTagRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace PoeData.Ggpk.GgpkRecords;
+
+/// <summary>
+/// Interface containing Read method for ggpk tag records.
+/// </summary>
+/// <typeparam name="T">type of the returned <see cref="IGgpkTagRecord"/>.</typeparam>
+internal interface IReadGgpkTagRecord<T>
+    where T : IGgpkTagRecord
+{
+    /// <summary>
+    /// Reads ggpk record data.
+    /// </summary>
+    /// <param name="ggpkReader">reader to read from.</param>
+    /// <param name="length">records length.</param>
+    /// <param name="offset">records offset.</param>
+    /// <returns>parsed ggpk record data.</returns>
+    public static abstract T Read(BinaryReader ggpkReader, int length, long offset);
+}
