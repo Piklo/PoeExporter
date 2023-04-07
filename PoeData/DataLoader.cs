bf5d086b18062ecb53b665cfb34edc1fa3448ca2
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-07 15:32:05 +0200

    added DataLoader

---
+++ PoeData/DataLoader.cs
@@ -0,0 +1,48 @@
+﻿using PoeData.Ggpk;
+using Serilog;
+
+namespace PoeData;
+
+/// <summary>
+/// Class used to load poe data.
+/// </summary>
+internal sealed class DataLoader : IDataLoader
+{
+    private readonly ILogger logger;
+    private readonly IConfig config;
+    private readonly IDataLoader dataLoader;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataLoader"/> class.
+    /// </summary>
+    /// <param name="config">Contains config data.</param>
+    /// <param name="logger">Contains logger used through the application.</param>
+    public DataLoader(IConfig config, ILogger logger)
+    {
+        this.logger = logger;
+        this.config = config;
+
+        if (IsStandaloneClient())
+        {
+            this.logger.Debug("detected standalone client");
+            dataLoader = new GgpkLoader(config, logger);
+        }
+        else
+        {
+            this.logger.Debug("detected steam client");
+            dataLoader = new SteamDataLoader(config, logger);
+        }
+    }
+
+    /// <inheritdoc/>
+    public byte[] GetFileBytes(string filePath)
+    {
+        return dataLoader.GetFileBytes(filePath);
+    }
+
+    private bool IsStandaloneClient()
+    {
+        var ggpkPath = Path.Combine(config.PoePath, "Content.ggpk");
+        return Path.Exists(ggpkPath);
+    }
+}
