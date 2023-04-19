32670f6df855333f033a8f1b2bc74aa86c4bcce8
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-19 19:33:22 +0200

    added System.CommandLine to PoeExporter reworked Program.cs in PoeExporter added basic JsonCommands

---
+++ PoeExporter/JsonExporters/JsonCommands.cs
@@ -0,0 +1,70 @@
+﻿using Serilog;
+using System.CommandLine;
+
+namespace PoeExporter.JsonExporters;
+
+/// <summary>
+/// Class which adds json exporter commands to a <see cref="Command"/>.
+/// </summary>
+internal static class JsonCommands
+{
+    /// <summary>
+    /// Adds json commands.
+    /// </summary>
+    /// <param name="specificationWrapper">specification wrapper.</param>
+    /// <param name="logger">logger.</param>
+    /// <param name="command">command to which the method adds subcommands.</param>
+    public static void AddCommands(SpecificationWrapper specificationWrapper, ILogger logger, Command command)
+    {
+        var jsonCommand = new Command("json", "exports data to json");
+        command.Add(jsonCommand);
+
+        AddExportAllDatFiles(specificationWrapper, logger, jsonCommand);
+
+        AddExportListedDatFiles(specificationWrapper, logger, jsonCommand);
+    }
+
+    private static void AddExportAllDatFiles(SpecificationWrapper specificationWrapper, ILogger logger, Command jsonCommand)
+    {
+        var allDatFilesCommand = new Command("allDatFiles", "exports all dat files");
+        jsonCommand.Add(allDatFilesCommand);
+
+        allDatFilesCommand.SetHandler(
+            () =>
+            {
+                var datExporter = new DatJsonExporter(logger, specificationWrapper.GetOrCreateSpecification());
+                logger.Information("exporing all dat files");
+                datExporter.Run();
+            });
+    }
+
+    private static void AddExportListedDatFiles(SpecificationWrapper specificationWrapper, ILogger logger, Command jsonCommand)
+    {
+        var listedDatFilesCommand = new Command("datFiles", "exports listed dat files");
+        jsonCommand.Add(listedDatFilesCommand);
+
+        var datFiles = new Option<string[]>("--files", "exports listed dat files (space separated)")
+        {
+            AllowMultipleArgumentsPerToken = true,
+            IsRequired = true,
+
+        };
+        listedDatFilesCommand.Add(datFiles);
+
+        listedDatFilesCommand.SetHandler(
+            (datFiles) =>
+            {
+                if (datFiles.Length != 0)
+                {
+                    var datExporter = new DatJsonExporter(logger, specificationWrapper.GetOrCreateSpecification());
+                    logger.Information("exporing listed dat files: {files}", string.Join(", ", datFiles));
+                    logger.Error("not yet implemented");
+                }
+                else
+                {
+                    logger.Error("passed 0 files to export");
+                }
+            },
+            datFiles);
+    }
+}
--- PoeExporter/Program.cs
+++ PoeExporter/Program.cs
@@ -1,93 +1,37 @@
 ﻿using Microsoft.Extensions.Configuration;
-using PoeData.Specifications;
-using PoeExporter.WikiExporters;
+using PoeExporter;
+using PoeExporter.JsonExporters;
 using Serilog;
 using Serilog.Core;
+using System.CommandLine;
 
-namespace PoeExporter;
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("config.json", optional: false);
 
-internal sealed class Program
+IConfiguration config = builder.Build();
+
+var parsedConfig = config.Get<Config>();
+if (parsedConfig is null)
 {
-    static async Task Main()
-    {
-
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("config.json", optional: false);
-
-        IConfiguration config = builder.Build();
-
-        var parsedConfig = config.Get<Config>();
-        if (parsedConfig is null) throw new ArgumentNullException(nameof(parsedConfig));
-
-        var levelSwitch = new LoggingLevelSwitch
-        {
-            MinimumLevel = Serilog.Events.LogEventLevel.Verbose
-        };
-        var logger = new LoggerConfiguration()
-            .MinimumLevel.ControlledBy(levelSwitch)
-            .WriteTo.Console()
-            .CreateLogger();
-
-        //using var client = new WikiClient("https://www.poewiki.net/w/api.php", logger);
-        //await client.GetPageContent("Module:Blight/blight crafting recipes").ConfigureAwait(true);
-
-        var specification = new Specification(parsedConfig, logger);
-
-        //var abyss = specification.GetAbyssObjectsDat();
-
-
-        //var serialized = System.Text.Json.JsonSerializer.Serialize(abyss[^1], new System.Text.Json.JsonSerializerOptions()
-        //{
-        //    WriteIndented = true,
-        //    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-        //});
-
-        //logger.Verbose(serialized);
-
-        var recipes = specification.LoadBlightCraftingRecipesDat();
-        var craftingItems = specification.LoadBlightCraftingItemsDat();
-        var baseItems = specification.LoadBaseItemTypesDat();
-
-        var serialized = System.Text.Json.JsonSerializer.Serialize(recipes[0], new System.Text.Json.JsonSerializerOptions()
-        {
-            WriteIndented = true,
-            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-        });
-
-        logger.Verbose(serialized);
-
-        var results = new List<BlightCraftingRecipesItems>();
-        foreach (var recipe in recipes)
-        {
-            for (var i = 0; i < recipe.BlightCraftingItemsKeys.Count; i++)
-            {
-                var ordinal = i + 1;
-                var recipeId = recipe.Id;
-
-                var blightCraftingItemKey = recipe.BlightCraftingItemsKeys[i];
-                var blightCraftingItem = craftingItems[blightCraftingItemKey];
-                var oil = blightCraftingItem.Oil;
-                if (oil is null)
-                {
-                    throw new ArgumentNullException("null oil");
-                }
-                var itemId = baseItems[(int)oil].Id;
-
-                var res = new BlightCraftingRecipesItems()
-                {
-                    ItemId = itemId,
-                    Ordinal = ordinal,
-                    RecipeId = recipeId
-                };
-
-                results.Add(res);
-            }
-        }
-
-        var formatted = BlightCraftingRecipesItems.ToLuaString(results);
-
-        File.WriteAllText("mine_blight_crafting_recipes_items.lua", formatted);
-    }
+    return;
 }
 
+var levelSwitch = new LoggingLevelSwitch
+{
+    MinimumLevel = Serilog.Events.LogEventLevel.Verbose,
+};
+var logger = new LoggerConfiguration()
+    .MinimumLevel.ControlledBy(levelSwitch)
+    .WriteTo.Console()
+    .CreateLogger();
+
+var specificationWrapper = new SpecificationWrapper(parsedConfig, logger);
+
+var rootCommand = new RootCommand("exports poe data.");
+JsonCommands.AddCommands(specificationWrapper, logger, rootCommand);
+
+// var wikiCommand = new Command("wiki", "exports data to wiki");
+// rootCommand.Add(wikiCommand);
+// var luaCommand = new Command("lua", "exports lua data");
+// wikiCommand.Add(luaCommand);
+// rootCommand.Invoke(args);
