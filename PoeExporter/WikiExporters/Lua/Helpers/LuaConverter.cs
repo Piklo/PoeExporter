37ba7f4a99c84368dacdcada30ac7fd7a35c1a13
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-21 19:53:25 +0200

    added LuaConverter

---
+++ PoeExporter/WikiExporters/Lua/Helpers/LuaConverter.cs
@@ -0,0 +1,39 @@
+﻿using PoeExporter.WikiExporters.Lua.Helpers;
+using System.Text;
+
+namespace PoeExporter.WikiExporters.Lua;
+
+/// <summary>
+/// Class used to convert c# classes to lua objects.
+/// </summary>
+internal static class LuaConverter
+{
+    /// <summary>
+    /// Converts a list of c# objects to lua string.
+    /// </summary>
+    /// <typeparam name="T">type of the object.</typeparam>
+    /// <param name="items">items.</param>
+    /// <returns>converted objects in lua string.</returns>
+    public static string ToLuaString<T>(IReadOnlyList<T> items)
+        where T : ILuaExporter<T>
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine("local data = {");
+        var baseIndentation = 1; // from the data indentation
+
+        foreach (var item in items)
+        {
+            var strings = item.ToLuaString(baseIndentation);
+            foreach (var line in strings)
+            {
+                builder.AppendLine($"{new string('\t', line.Indentation)}{line.Value}");
+            }
+        }
+
+        builder.AppendLine("}");
+        builder.AppendLine("return data");
+
+        var str = builder.ToString();
+        return str;
+    }
+}
