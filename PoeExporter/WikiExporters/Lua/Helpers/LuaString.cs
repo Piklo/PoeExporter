ff1f6cf63b1f3386a0b930e84b739d7441dd0d42
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-21 19:54:42 +0200

    added LuaString

---
+++ PoeExporter/WikiExporters/Lua/Helpers/LuaString.cs
@@ -0,0 +1,7 @@
+﻿namespace PoeExporter.WikiExporters.Lua.Helpers;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "StyleCop.CSharp.NamingRules",
+    "SA1313:Parameter names should begin with lower-case letter",
+    Justification = "its a record not a method with parameters :ICANT:")]
+internal readonly record struct LuaString(string Value, int Indentation);
