f8a6910bd55ecd0742242fbfe2a260c19878251e
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-21 19:54:13 +0200

    added LuaPropertyNameAttribute

---
+++ PoeExporter/WikiExporters/Lua/Helpers/LuaPropertyNameAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace PoeExporter.WikiExporters.Lua.Helpers;
+
+/// <summary>
+/// Attribute used to rename property names in lua converter.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+internal sealed class LuaPropertyNameAttribute : Attribute
+{
+    /// <summary>Gets overriden property name.</summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LuaPropertyNameAttribute"/> class.
+    /// </summary>
+    /// <param name="name">overriden name.</param>
+    public LuaPropertyNameAttribute(string name)
+    {
+        Name = name;
+    }
+}
