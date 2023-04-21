354b41fbd835a860a0d9e4ca5667076d13a2e087
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-21 19:52:58 +0200

    added ILuaExporter<T>

---
+++ PoeExporter/WikiExporters/Lua/Helpers/ILuaExporter.cs
@@ -0,0 +1,15 @@
+﻿namespace PoeExporter.WikiExporters.Lua.Helpers;
+
+/// <summary>
+/// Interface with lua exporter methods.
+/// </summary>
+/// <typeparam name="T">type of the exporter.</typeparam>
+internal interface ILuaExporter<T>
+{
+    /// <summary>
+    /// Converts the object to lua strings.
+    /// </summary>
+    /// <param name="currentIndentation">current indentation</param>
+    /// <returns>converted object in lua strings.</returns>
+    public LuaString[] ToLuaString(int currentIndentation = 0);
+}
