b4a6a4835af29c4aa458a32b3f194e6d21dba8ca
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-22 17:40:57 +0200

    updated BlightCraftingRecipesItems to implement ILuaExporter

--- PoeExporter/WikiExporters/BlightCraftingRecipesItems.cs
+++ PoeExporter/WikiExporters/BlightCraftingRecipesItems.cs
@@ -1,54 +1,24 @@
-﻿using System.Text;
-using System.Text.Json.Serialization;
+﻿using PoeExporter.WikiExporters.Lua.Helpers;
 
 namespace PoeExporter.WikiExporters;
 
-internal sealed class BlightCraftingRecipesItems
+/// <summary>
+/// Class representing values of https://www.poewiki.net/wiki/Module:Blight/blight_crafting_recipes.
+/// </summary>
+internal sealed partial class BlightCraftingRecipesItems : ILuaExporter<BlightCraftingRecipesItems>
 {
-    [JsonPropertyName("ordinal")]
+    /// <summary>Gets Ordinal.</summary>
+    [LuaPropertyName("ordinal")]
     public required int Ordinal { get; init; }
-    [JsonPropertyName("recipe_id")]
+
+    /// <summary>Gets RecipeId.</summary>
+    [LuaPropertyName("recipe_id")]
     public required string RecipeId { get; init; }
-    [JsonPropertyName("item_id")]
+
+    /// <summary>Gets ItemId.</summary>
+    [LuaPropertyName("item_id")]
     public required string ItemId { get; init; }
 
-    public string[] ToLuaStrings()
-    {
-        var res = new string[]
-        {
-            "{",
-            $"""ordinal = {Ordinal},""",
-            $"""recipe_id = "{RecipeId}",""",
-            $"""item_id = "{ItemId}",""",
-            "}"
-        };
-
-        return res;
-    }
-
-    public static string ToLuaString(List<BlightCraftingRecipesItems> items)
-    {
-        var builder = new StringBuilder();
-        builder.AppendLine("local data = {");
-        foreach (var item in items)
-        {
-            var strings = item.ToLuaStrings();
-
-            builder.AppendLine($"\t{strings[0]}");
-
-            for (var i = 1; i < strings.Length - 1; i++)
-            {
-                var str = strings[i];
-                builder.AppendLine($"\t\t{str}");
-            }
-
-            builder.AppendLine($"\t{strings[^1]},");
-
-        }
-        builder.AppendLine("}");
-        builder.AppendLine("return data");
-
-        var result = builder.ToString();
-        return result;
-    }
+    /// <inheritdoc/>
+    public partial LuaString[] ToLuaStrings(int currentIndentation = 0);
 }
