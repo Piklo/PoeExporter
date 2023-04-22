081b786fdc14c30cf90f0b477ae69dbacdcb9fee
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-22 18:19:07 +0200

    moved ToLuaStringsGenerator to LuaGenerator directory

--- PoeExporterGenerator/LuaGenerator/ToLuaStringsGenerator.cs
+++ PoeExporterGenerator/LuaGenerator/ToLuaStringsGenerator.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PoeExporterGenerator;
+namespace PoeExporterGenerator.LuaGenerator;
 
 /// <summary>
 /// Class used to generate to lua strings methods.
