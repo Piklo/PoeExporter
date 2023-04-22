a7a6564e1778d8eebdeddceff6ee1bd2820281a8
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-22 17:11:03 +0200

    delete GenerateToLuaStringsAttribute

--- PoeExporter/WikiExporters/Lua/Helpers/GenerateToLuaStringsAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PoeExporter.WikiExporters.Lua.Helpers;
-
-/// <summary>
-/// Attribute used to auto generate <see cref="ILuaExporter{T}.ToLuaStrings(int)"/> methods.
-/// </summary>
-[AttributeUsage(AttributeTargets.Method)]
-internal sealed class GenerateToLuaStringsAttribute : Attribute
-{
-}
