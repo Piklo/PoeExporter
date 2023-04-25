6fcf63da2f61b1a17d31b2fdc7562f795eb72182
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-25 17:37:31 +0200

    added BlightCraftingRecipes lua exporter

---
+++ PoeExporter/WikiExporters/Lua/BlightCraftingRecipes.cs
@@ -0,0 +1,24 @@
+﻿using PoeExporter.WikiExporters.Lua.Helpers;
+
+namespace PoeExporter.WikiExporters.Lua;
+
+/// <summary>
+/// Class representing values of https://www.poewiki.net/wiki/Module:Blight/blight_crafting_recipes
+/// </summary>
+internal sealed class BlightCraftingRecipes : ILuaExporter<BlightCraftingRecipes>
+{
+    /// <inheritdoc/>
+    public string PageName { get; } = "blight_crafting_recipes";
+
+    /// <summary>Gets id.</summary>
+    [LuaPropertyName("id")]
+    public required string Id { get; init; }
+
+    /// <summary>Gets passive id.</summary>
+    [LuaPropertyName("passive_id")]
+    public required string PassiveId { get; init; }
+
+    /// <summary>Gets type.</summary>
+    [LuaPropertyName("type")]
+    public required string Type { get; init; }
+}
