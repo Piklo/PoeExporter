a41d95e5eeb2649ed9653b07f1bcc4ea1ee5a1f3
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-26 16:03:19 +0200

    added BlightTowers lua exporter

---
+++ PoeExporter/WikiExporters/Lua/Blight/BlightTowers.cs
@@ -0,0 +1,40 @@
+﻿using PoeExporter.WikiExporters.Lua.Helpers;
+
+namespace PoeExporter.WikiExporters.Lua.Blight;
+
+/// <summary>
+/// Class representing values of https://www.poewiki.net/wiki/Module:Blight/blight_towers.
+/// </summary>
+internal class BlightTowers : ILuaExporter<BlightTowers>
+{
+    /// <inheritdoc />
+    public string PageName { get; } = "blight_towers";
+
+    /// <summary>Gets id.</summary>
+    [LuaPropertyName("id")]
+    public required string Id { get; init; }
+
+    /// <summary>Gets name.</summary>
+    [LuaPropertyName("name")]
+    public required string Name { get; init; }
+
+    /// <summary>Gets description.</summary>
+    [LuaPropertyName("description")]
+    public required string Description { get; init; }
+
+    /// <summary>Gets tier.</summary>
+    [LuaPropertyName("tier")]
+    public required string? Tier { get; init; }
+
+    /// <summary>Gets radius.</summary>
+    [LuaPropertyName("radius")]
+    public required int Radius { get; init; }
+
+    /// <summary>Gets icon.</summary>
+    [LuaPropertyName("icon")]
+    public required string? Icon { get; init; }
+
+    /// <summary>Gets cost.</summary>
+    [LuaPropertyName("cost")]
+    public required int Cost { get; init; }
+}
