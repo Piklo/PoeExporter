6fdde5a29ad14ca966ef66dbe9c3247b5fd21f79
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-29 13:37:01 +0200

    added AddWikiExporterAttribute

---
+++ PoeExporter/WikiExporters/AddWikiExporterAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace PoeExporter.WikiExporters;
+
+/// <summary>
+/// Attribute used add wiki exporters.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+internal sealed class AddWikiExporterAttribute : Attribute
+{
+    /// <summary>Gets exporter.</summary>
+    public Type[] Exporters { get; }
+
+    /// <summary>Gets exporter.</summary>
+    public string[] Aliases { get; }
+
+    /// <summary>Gets exporter.</summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddWikiExporterAttribute"/> class.
+    /// </summary>
+    /// <param name="exporters">exporters.</param>
+    /// <param name="aliases">option aliases.</param>
+    /// <param name="description">option description.</param>
+    public AddWikiExporterAttribute(Type[] exporters, string[] aliases, string description)
+    {
+        Exporters = exporters;
+        Aliases = aliases;
+        Description = description;
+    }
+}
