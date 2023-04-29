7d835745a8a89bfe75479c3d594f1fc2fbe15687
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-29 13:35:46 +0200

    added IExporter<T>

---
+++ PoeExporter/WikiExporters/IExporter.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace PoeExporter.WikiExporters;
+
+/// <summary>
+/// Interface containing exporter methods.
+/// </summary>
+/// <typeparam name="T">type of the exporter</typeparam>
+internal interface IExporter<T>
+{
+    /// <summary>
+    /// Creates instance of <see cref="IExporter{T}"/>.
+    /// </summary>
+    /// <param name="specificationWrapper">specification wrapper.</param>
+    /// <param name="logger">logger.</param>
+    /// <returns>instance of <see cref="IExporter{T}"/>.</returns>
+    static abstract T Create(SpecificationWrapper specificationWrapper, ILogger logger);
+
+    /// <summary>
+    /// Exports data.
+    /// </summary>
+    /// <returns>exporter string.</returns>
+    string Export();
+}
