0279c635d93b9152b3e9755c7519bcbb1ead8ad1
Piklo <16057987+Piklo@users.noreply.github.com>
2023-04-29 13:52:37 +0200

    added WikiCommands

---
+++ PoeExporter/WikiExporters/WikiCommands.cs
@@ -0,0 +1,26 @@
+﻿using PoeExporter.WikiExporters.Lua.Blight;
+using Serilog;
+using System.CommandLine;
+
+namespace PoeExporter.WikiExporters;
+
+/// <summary>
+/// Class which adds wiki exporter commands to a <see cref="Command"/>.
+/// </summary>
+[AddWikiExporter(
+    new Type[] { typeof(BlightCraftingRecipesExporter) },
+    new string[] { "--luablight", "--blight" },
+    "Exports lua blight data")]
+internal sealed partial class WikiCommands
+{
+    /// <summary>
+    /// Adds wiki commands.
+    /// </summary>
+    /// <param name="specificationWrapper">specification wrapper.</param>
+    /// <param name="logger">logger.</param>
+    /// <param name="rootCommand">command to which the method adds subcommands.</param>
+    public static void AddCommands(SpecificationWrapper specificationWrapper, ILogger logger, Command rootCommand)
+    {
+        _ = new WikiCommands(specificationWrapper, logger, rootCommand);
+    }
+}
