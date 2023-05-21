779e06aaf48e15b4158a0feb018b3541e2a3cf45
Piklo <16057987+Piklo@users.noreply.github.com>
2023-05-21 09:15:59 +0200

    added LineOfCode record struct

---
+++ SpecificationGenerator/LineOfCode.cs
@@ -0,0 +1,26 @@
+﻿namespace SpecificationGenerator;
+
+internal readonly record struct LineOfCode(string Value, int Indentation)
+{
+    /// <summary>
+    /// Splits a string of code into list of <see cref="LineOfCode"/>.
+    /// </summary>
+    /// <param name="code">code to split</param>
+    /// <returns>split code.</returns>
+    public static IReadOnlyList<LineOfCode> Split(string code)
+    {
+        var resulsts = new List<LineOfCode>();
+        var lines = code.Split("\r\n");
+
+        foreach (var line in lines)
+        {
+            var trimmed = line.TrimStart();
+            var difference = line.Length - trimmed.Length;
+            var indentation = difference / 4;
+            var parsedLine = new LineOfCode(trimmed, indentation);
+            resulsts.Add(parsedLine);
+        }
+
+        return resulsts;
+    }
+}
