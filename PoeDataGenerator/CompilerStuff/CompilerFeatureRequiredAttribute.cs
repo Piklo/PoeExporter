441509d770dc13b50fb997b4ed6445fba8ba2439
Piklo <16057987+Piklo@users.noreply.github.com>
2023-06-08 14:25:30 +0200

    silenced IDE0060 in CompilerFeatureRequiredAttribute

--- PoeDataGenerator/CompilerStuff/CompilerFeatureRequiredAttribute.cs
+++ PoeDataGenerator/CompilerStuff/CompilerFeatureRequiredAttribute.cs
@@ -9,7 +9,9 @@ public class CompilerFeatureRequiredAttribute : Attribute
     /// Initializes a new instance of the <see cref="CompilerFeatureRequiredAttribute"/> class.
     /// </summary>
     /// <param name="name">name.</param>
+#pragma warning disable IDE0060 // Remove unused parameter
     public CompilerFeatureRequiredAttribute(string name)
+#pragma warning restore IDE0060 // Remove unused parameter
     {
     }
 }
