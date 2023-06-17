d55f44b3e40450e02615c651b069d9748aaac56c
Piklo <16057987+Piklo@users.noreply.github.com>
2023-06-17 12:45:53 +0200

    added StatDescriptionsHelper.IsSpace() updated docs in StatDescriptionsHelper

--- PoeData/Specifications/StatDescriptions/StatDescriptionsHelper.cs
+++ PoeData/Specifications/StatDescriptions/StatDescriptionsHelper.cs
@@ -10,7 +10,7 @@ internal static class StatDescriptionsHelper
     /// </summary>
     /// <param name="span">span with unicode bytes.</param>
     /// <param name="index">index to check.</param>
-    /// <returns><see langword="true"/> if they translate to the newline character, <see langword="false"/> otherwise.</returns>
+    /// <returns><see langword="true"/> if next two bytes translate to the newline character, <see langword="false"/> otherwise.</returns>
     internal static bool IsNewLine(ReadOnlySpan<byte> span, int index)
     {
         if (index + 1 >= span.Length)
@@ -26,7 +26,7 @@ internal static class StatDescriptionsHelper
     /// </summary>
     /// <param name="span">span with unicode bytes.</param>
     /// <param name="index">index to check.</param>
-    /// <returns><see langword="true"/> if they translate to the tab character, <see langword="false"/> otherwise.</returns>
+    /// <returns><see langword="true"/> if next two bytes translate to the tab character, <see langword="false"/> otherwise.</returns>
     internal static bool IsTab(ReadOnlySpan<byte> span, int index)
     {
         if (index + 1 >= span.Length)
@@ -35,5 +35,21 @@ internal static class StatDescriptionsHelper
         }
 
         return span[index] == '\t' && span[index + 1] == 0;
+    }
+
+    /// <summary>
+    /// Checks if next two bytes at the index translate to space character.
+    /// </summary>
+    /// <param name="span">span with unicode bytes.</param>
+    /// <param name="index">index to check.</param>
+    /// <returns><see langword="true"/> if next two bytes translate to the space character, <see langword="false"/> otherwise.</returns>
+    internal static bool IsSpace(ReadOnlySpan<byte> span, int index)
+    {
+        if (index + 1 >= span.Length)
+        {
+            return false;
+        }
+
+        return span[index] == ' ' && span[index + 1] == 0;
     }
 }
