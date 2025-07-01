aa90c9d9c418097d4544d7bf254bbde0a34bc77c
Piklo <16057987+Piklo@users.noreply.github.com>
2025-07-01 19:39:06 +0200

    WIP ColumnHelpers

---
+++ PoeData.Generator/ColumnHelpers.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace PoeData.Generator;
+
+internal static class ColumnHelpers
+{
+    public static string GetColumnType(Column column)
+    {
+        return column switch
+        {
+            { Array: false, Type: "bool" } => "bool",
+            { Array: true, Type: "bool" } => "bool[]",
+            { Array: false, Type: "i16" } => "short",
+            { Array: true, Type: "i16" } => "short[]",
+            { Array: false, Type: "i32" } => "int",
+            { Array: true, Type: "i32" } => "int[]",
+            { Array: false, Type: "u16" } => "ushort",
+            { Array: true, Type: "ui16" } => "ushort[]",
+            { Array: false, Type: "u32" } => "uint",
+            { Array: true, Type: "u32" } => "uint[]",
+            { Array: false, Type: "f32" } => "float",
+            { Array: true, Type: "f32" } => "float[]",
+            { Array: false, Type: "string" } => "string",
+            { Array: true, Type: "string" } => "string[]",
+            _ => throw new NotImplementedException($"Not implemented column type.")
+        };
+    }
+}
